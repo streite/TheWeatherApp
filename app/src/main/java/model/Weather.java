07ca0d04400c1af2025e6122f0e95d08576a0a1e
streitenberger@gmail.com <Jjmania8*>
2017-03-10 17:16:08 -0800

    Initial commit

---
+++ app/src/main/java/model/Weather.java
@@ -0,0 +1,17 @@
+package model;
+
+/**
+ * Created by paulodichone on 3/11/15.
+ */
+public class Weather {
+    public Place place;
+    public String iconData;
+    public CurrentCondition currentCondition = new CurrentCondition();
+    public Temperature temperature = new Temperature();
+    public Wind wind = new Wind();
+    public Rain rain = new Rain();
+    public Snow snow = new Snow();
+    public Clouds clouds = new Clouds();
+
+}
+
